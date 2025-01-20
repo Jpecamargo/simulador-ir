4f4f84c066be52c907dcdce9f844fee28b2ada8c
jpecamargo <jpecamargo98@gmail.com>
2025-01-19 21:07:47 -0300

    refactor: ajusta formatação do arquivo data-source.ts

--- apps/backend/data-source.ts
+++ apps/backend/data-source.ts
@@ -1,25 +1,16 @@
-import { DataSource } from "typeorm"
+import { DataSource } from 'typeorm';
 
 const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'simulador_ir',
-    synchronize: false,
-    entities: [`${__dirname}/**/*.entity{.ts,*js}`],
-    migrations: [`${__dirname}/migrations/*{.ts,*js}`],
-    migrationsRun: true,
-  });
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'simulador_ir',
+  synchronize: false,
+  entities: [`${__dirname}/**/*.entity{.ts,*js}`],
+  migrations: [`${__dirname}/migrations/*{.ts,*js}`],
+  migrationsRun: true,
+});
 
-  AppDataSource.initialize()
-  .then(() => {
-    console.log('Data Source has been initialized!');
-    console.log('Migrations:', AppDataSource.migrations);
-  })
-  .catch((err) => {
-    console.error('Error during Data Source initialization', err);
-  });
-
-  export default AppDataSource;
+export default AppDataSource;
