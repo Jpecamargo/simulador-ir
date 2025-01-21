3dbd6ff6cf262f1b361d78106070821922146f44
jpecamargo <jpecamargo98@gmail.com>
2025-01-21 20:20:53 -0300

    feat: adiciona arquivo .env.example e configurações de banco de dados a partir de variáveis de ambiente

--- apps/backend/src/db/db.module.ts
+++ apps/backend/src/db/db.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'process';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'simulador_ir',
+      host: process.env.DB_HOST || 'postgres',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'simulador_ir',
       autoLoadEntities: true,
       synchronize: false,
       entities: [`${__dirname}/**/*.entity{.ts,.js}`],
