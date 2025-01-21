e5bc835abc885e0fafa400e4d3639f89b255446d
jpecamargo <jpecamargo98@gmail.com>
2025-01-21 09:34:52 -0300

    feat: adiciona destaque de botão no cabeçalho com base na rota atual

--- apps/frontend/src/components/Header/index.tsx
+++ apps/frontend/src/components/Header/index.tsx
@@ -4,9 +4,17 @@ import Link from "next/link";
 import Image from "next/image";
 import Menu from "../../../public/menu-icon.svg";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = usePathname();
+  const buttonStyle = {
+    selected:
+      "text-white bg-blue-700  md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500",
+    default:
+      "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
+  };
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 w-full">
@@ -26,12 +34,19 @@ export default function Header() {
         >
           <Image src={Menu} alt="" />
         </button>
-        <div className={`${isMenuOpen? '': 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+        <div
+          className={`${isMenuOpen ? "" : "hidden"} w-full md:block md:w-auto`}
+          id="navbar-default"
+        >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
                 href="/declaracao"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                className={`block py-2 px-3 rounded ${
+                  pathname == "/declaracao"
+                    ? buttonStyle.selected
+                    : buttonStyle.default
+                }`}
                 aria-current="page"
               >
                 Nova declaração
@@ -40,7 +55,11 @@ export default function Header() {
             <li>
               <Link
                 href="/historico"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={`block py-2 px-3 rounded ${
+                  pathname == "/historico"
+                    ? buttonStyle.selected
+                    : buttonStyle.default
+                }`}
               >
                 Histórico
               </Link>
