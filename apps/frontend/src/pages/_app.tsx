39e7079c3e694d365e522498b51f2c0760c1c8b0
jpecamargo <jpecamargo98@gmail.com>
2025-01-21 16:15:23 -0300

    fix: ajusta a importação do arquivo CSS global

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { AuthProvider } from "../../contexts/authContext";
-import "./globals.css"; // Import the global CSS file
+import "./globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
